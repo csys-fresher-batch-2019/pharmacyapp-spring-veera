9a36ab87f4b59c31d7208425da19e676107c1408
VeeraDinesh <veeradinesh7080@gmail.com>
2020-03-10 17:02:17 +0530

    update

--- src/main/java/com/chainsys/pharmacyapp/controller/ProductController.java
+++ src/main/java/com/chainsys/pharmacyapp/controller/ProductController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.chainsys.pharmacyapp.dao.ProductDAO;
-import com.chainsys.pharmacyapp.dao.impl.ProductImplementation;
+import com.chainsys.pharmacyapp.dao.impl.ProductDAOImpl;
 import com.chainsys.pharmacyapp.model.Product;
+
+
 
 @RestController
 @RequestMapping("api")
@@ -17,7 +20,7 @@ public class ProductController {
 
 	@GetMapping("/displayProduct")
 	public List<Product> displayProduct() throws Exception {
-		ProductDAO obj = new ProductImplementation();
+		ProductDAO obj = new ProductDAOImpl();
 		return obj.displayProduct();
 	}
 
@@ -25,13 +28,13 @@ public class ProductController {
 	public int addProductDetails(@RequestParam("product_id") int productId,
 			@RequestParam("product_name") String productName, @RequestParam("product_type") String productType,
 			@RequestParam("cost") int cost) throws Exception {
-		ProductDAO obj = new ProductImplementation();
+		ProductDAO obj = new ProductDAOImpl();
 		Product p = new Product();
 		p.setProductId(productId);
 		p.setProductName(productName);
 		p.setProductType(productType);
 		p.setCost(cost);
-		return obj.addProductDetails(p);
+		return obj.save(p);
 	}
 	// @PostMapping("/AdminLogin")
 	// public boolean AdminLogin(@RequestParam("adminname") String
--- src/main/java/com/chainsys/servlet/AddProductDetails.java
+++ src/main/java/com/chainsys/servlet/AddProductDetails.java
@@ -55,7 +55,6 @@ public class AddProductDetails extends HttpServlet {
 
 			}
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
